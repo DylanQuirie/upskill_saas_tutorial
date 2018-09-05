68a99c7b021f3ddc3bc8e9677ef42c13963c2312
Dylan Quirie <dylanquirie@gmail.com>
2018-09-05 00:37:14 +0000

    Added validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-  
+  #Blueprint
+  validates :name, presence=true # Checking if the name field has been filled out
+  validates :email, presence=true # Checking if the email field has been filled out
+  validates :comments, presence=true # Checking if the comment field has been filled out
 end
